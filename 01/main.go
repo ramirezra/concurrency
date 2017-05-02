02b96c2d3ecf20964ac0c7a2ec09d4274e3cdfef
ramirezra <robinson@camrpc.com>
2017-05-02 01:23:11 -0400

    Solved Problem 1

--- 01/main.go
+++ 01/main.go
@@ -7,41 +7,60 @@ import (
 	"time"
 )
 
+// GetReady function
+func GetReady(done chan bool, name string) {
+	fmt.Println(name + " started getting ready.")
+	seed := rand.New(rand.NewSource(time.Now().UnixNano()))
+	duration := seed.Intn(30) + 60
+
+	fmt.Println(name + " spent " + strconv.Itoa(duration) + " seconds getting ready.")
+	done <- true
+}
+
+func SetAlarm(done chan bool) {
+	fmt.Println("Arming the alarm.")
+	time.Sleep(time.Second * 2)
+	fmt.Println("The alarm is set.")
+	done <- true
+}
+
+func Shoes(done chan bool, name string) {
+	fmt.Println(name + " started putting on shoes.")
+	seed := rand.New(rand.NewSource(time.Now().UnixNano()))
+	duration := seed.Intn(10) + 35
+	fmt.Println(name + " spend " + strconv.Itoa(duration) + " seconds putting on shoes.")
+	done <- true
+}
+
 func main() {
-	// Create an buffered channel
+	fmt.Println("Let's go for a walk!")
 
-	channel := make(chan int)
-	message := make(chan string)
-	done := make(chan bool)
+	done := make(chan bool, 2)
 
-	fmt.Println("Let's go for a walk.")
+	go GetReady(done, "Bob")
+	go GetReady(done, "Alice")
+	<-done
+	<-done
+	go SetAlarm(done)
+	go Shoes(done, "Bob")
+	go Shoes(done, "Alice")
 
-	name := []string{"Bob", "Alice"}
+	time.Sleep(time.Second * 1)
+	<-done
+	<-done
+	fmt.Println("Exiting and locking the door.")
+	<-done
+	close(done)
 
-	for i := 0; i < 2; i++ {
-		go func() {
-			channel <- i
-			fmt.Printf(name[i] + " started getting ready.\n")
-
-			seed := rand.NewSource(time.Now().Unix())
-			count := rand.New(seed)
-			duration := count.Intn(60) + 30
-			message <- (name[i] + " spent " + strconv.Itoa(duration) + " seconds getting ready.")
-			done <- true
-		}()
+	for text := range done {
+		fmt.Println(text)
 	}
-	go func() {
-		for i := 0; i < 2; i++ {
-			<-done
-		}
-		close(channel)
-		close(message)
-	}()
-	time.Sleep(4 * time.Second)
 
-	for n := range message {
-		fmt.Println(n)
-	}
-	fmt.Println("Arming alarm.")
-
+	// Bob started putting on shoes
+	// Alarm is counting down.
+	// Alice started putting on shoes
+	// Alice spent 37 seconds putting on shoes
+	// Bob spent 39 seconds putting on shoes
+	// Exiting and locking the door.
+	// Alarm is armed.
 }
--- exercise01/main.go
+++ exercise01/main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+// GetReady function
+func GetReady(done chan bool, name string) {
+	fmt.Println(name + " started getting ready.")
+	seed := rand.New(rand.NewSource(time.Now().UnixNano()))
+	duration := seed.Intn(30) + 60
+
+	fmt.Println(name + " spent " + strconv.Itoa(duration) + " seconds getting ready.")
+	done <- true
+}
+
+func SetAlarm(done chan bool) {
+	fmt.Println("Arming the alarm.")
+	time.Sleep(time.Second * 2)
+	fmt.Println("The alarm is set.")
+	done <- true
+}
+
+func Shoes(done chan bool, name string) {
+	fmt.Println(name + " started putting on shoes.")
+	seed := rand.New(rand.NewSource(time.Now().UnixNano()))
+	duration := seed.Intn(10) + 35
+	fmt.Println(name + " spend " + strconv.Itoa(duration) + " seconds putting on shoes.")
+	done <- true
+}
 
 func main() {
 	fmt.Println("Let's go for a walk!")
+
+	done := make(chan bool, 2)
+
+	go GetReady(done, "Bob")
+	go GetReady(done, "Alice")
+	<-done
+	<-done
+	go SetAlarm(done)
+	go Shoes(done, "Bob")
+	go Shoes(done, "Alice")
+
+	time.Sleep(time.Second * 1)
+	<-done
+	<-done
+	fmt.Println("Exiting and locking the door.")
+	<-done
+	close(done)
+
+	for text := range done {
+		fmt.Println(text)
+	}
+
+	// Bob started putting on shoes
+	// Alarm is counting down.
+	// Alice started putting on shoes
+	// Alice spent 37 seconds putting on shoes
+	// Bob spent 39 seconds putting on shoes
+	// Exiting and locking the door.
+	// Alarm is armed.
 }
