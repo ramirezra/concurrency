188da606f55989dad9c6b1f45269c70d998abf99
Robinson Ramirez <robinson@camrpc.com>
2017-05-09 15:29:43 -0400

    Cafe start.

--- 03_ChatCafe/FirstAttempt/main.go
+++ 03_ChatCafe/FirstAttempt/main.go
@@ -12,22 +12,43 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-func main() {
-	fmt.Println(runtime.NumCPU())
-	tourist := make(chan int)
-	for i := 1; i <= 25; i++ {
-		tourist <- i
-	}
-	close(tourist)
-
-	// go terminal(tourist)
-	for chat := range tourist {
-		fmt.Println(chat)
-	}
-}
+// func main() {
+// 	fmt.Println(runtime.NumCPU())
+// 	tourist := make(chan int)
+// 	for i := 1; i <= 25; i++ {
+// 		tourist <- i
+// 	}
+// 	close(tourist)
+//
+// 	// go terminal(tourist)
+// 	for chat := range tourist {
+// 		fmt.Println(chat)
+// 	}
+// }
 
 // func terminal(tourist chan int) {
 // 	for chat := range tourist {
 // 		fmt.Println(chat)
 // 	}
 // }
+
+type Request struct {
+	args       []int
+	f          func([]int) int
+	resultChan chan int
+}
+
+func sum(a []int) (s int) {
+	for _, v := range a {
+		s += v
+	}
+	return
+}
+
+func main() {
+	request := &Request{[]int{3, 4, 5}, sum, make(chan int)}
+
+	clientRequests <- request
+
+	fmt.Printf("answer: %d\n", <-request.resultChan)
+}
